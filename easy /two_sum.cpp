e09562b21bbd2edbe7451fcf109431e04c95b929
bhomik005 <164642634+bhomik005@users.noreply.github.com>
2024-04-16 14:43:42 +1000

    Update two_sum.cpp

--- easy /two_sum.cpp
+++ easy /two_sum.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // O(n^2) time | O(1) space - not optimal
+        // Brute force solution 
+        // O(n^2) time | O(1) space
+        // for(int i = 0; i < nums.size() - 1; ++i) {
+        //     for(int j = i + 1; j < nums.size(); ++j) {
+        //         if(nums[i] + nums[j] == target) return {i, j};
+        //     }
+        // }
+        // return {};
+
+        // make use of hashmap data structure to solve this problem in O(n) time
         // O(n) time | O(n) space
         unordered_map<int,int> prevNums;
-        vector<int> res(2);
         for(int i = 0; i < nums.size(); ++i) {
             int potentialMatch = target - nums[i];
-            auto itr = prevNums.find(potentialMatch);
-            if(itr != prevNums.end()) {
-                res = {itr->second, i};
-                break;
-            }
-            else prevNums.insert(make_pair(nums[i], i));
+            if(prevNums.find(potentialMatch) != prevNums.end()) return {i, prevNums[potentialMatch]};
+            else prevNums[nums[i]] = i;
         }
-        return res;
+        return {};
+        
     }
 };
